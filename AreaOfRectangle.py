5d602269a4157524b3809c62ed481627c9172923
yaswanth reddy <sivayaswanth59@gmail.com>
2025-08-13 07:55:26 +0530

    Area of Rectangle

---
+++ AreaOfRectangle.py
@@ -0,0 +1,10 @@
+# Area of Rectangle
+
+length = int(input('Enter the Length Of the Rectangle :'))
+
+breadth = int(input('Enter the Breadth of the Rectangle :'))
+
+# Calculating the Area Of Rectangle
+areaOfRectangle = length*breadth
+print(f"The Area of the Rectangle : {areaOfRectangle} 'meters'")
+
