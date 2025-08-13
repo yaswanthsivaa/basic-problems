dba6ff1f475528066298f2fb240fa3f9cd0f9976
yaswanth reddy <sivayaswanth59@gmail.com>
2025-08-13 07:52:35 +0530

    Sum of Two Numbers

---
+++ Sum.py
@@ -0,0 +1,7 @@
+# Sum of Two numbers
+
+nums = input('Enter the two numbers with (,) separated :').split(',')
+
+sum = int(nums[0]) + int(nums[1])
+
+print(f"The Sum of {nums[0]} and {nums[1]} is :{sum}")
