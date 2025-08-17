83c3aff0c580dc0ab79079a4aea5bd3e5972a7bd
yaswanth reddy <sivayaswanth59@gmail.com>
2025-08-17 08:41:22 +0530

    Celsius_to_Fahrenheit.java

--- Celsius_to_Fahrenheit.java
+++ Celsius_to_Fahrenheit.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class CelsiusToFahrenheit {
+public class Celsius_to_Fahrenheit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
