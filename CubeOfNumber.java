0ca534e43f1d1d0552af1ea2672ce8e887a53255
yaswanthsivaa <yyaswanthreddyy@gmail.com>
2025-08-17 08:23:41 +0530

    update

---
+++ CubeOfNumber.java
@@ -0,0 +1,20 @@
+public class CubeOfNumber {
+  
+  public static void main(String[] args){
+    
+    int num = 9;
+
+    new CubeOfNumber().cubeOfNum(num);
+
+    System.gc();
+  }
+
+  public void cubeOfNum(int num){
+    
+    // calculating Cube of Number
+
+    int cubeOfNum = num*num*num;
+
+    System.out.println("Cube of a Number is :"+cubeOfNum);
+  }
+}
