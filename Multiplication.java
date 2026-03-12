172ee9313c9fbebf36f0ebbea5167218f6a83957
yaswanth reddy <sivayaswanth59@gmail.com>
2026-03-13 00:08:05 +0530

    Multiplication

--- Multiplication.java
+++ Multiplication.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Multiplication {
   
-  
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the number to print its table :");
