7aa0c8ab6b6d9d66514ad09571d1832d337e9aff
yaswanth reddy <sivayaswanth59@gmail.com>
2026-05-05 18:27:52 +0530

    Multiplication

--- Multiplication.java
+++ Multiplication.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 public class Multiplication {
-  
+ 
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the number to print its table :");
