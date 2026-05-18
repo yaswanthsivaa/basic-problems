0dc65b56f97820b2cfd088c6c83224d7d60e0378
yaswanth reddy <sivayaswanth59@gmail.com>
2026-05-18 20:44:59 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -19,7 +19,7 @@ public class LeapYear {
   public static void main(String[] args){
 
     Scanner sc = new Scanner(System.in);
-
+ 
     System.out.print("Enter the Year :");
     int year = sc.nextInt();
    
