8375c9860dcb8cbdf8daf596bc87e041b5f86e0a
yaswanth reddy <sivayaswanth59@gmail.com>
2026-05-26 16:19:42 +0530

    Add Fibonacci series implementation in Python
    Implement Fibonacci series calculation using a loop.

---
+++ FibonacciProblem.py
@@ -0,0 +1,20 @@
+# Fibonacci Series Using Loop
+  # Time Complexity = O(N)
+  # Space Complexity = O(1)
+
+prev = 1
+prevPrev = 0
+
+for i in range(0, 11):
+    if i == 0:
+        print(prevPrev)
+        continue
+    
+    if i == 1:
+        print(prev)
+        continue
+
+    temp = prev + prevPrev
+    prevPrev = prev
+    prev = temp
+    print(temp)
