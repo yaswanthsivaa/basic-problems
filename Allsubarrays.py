eda78c3cef19f8cd0f2dc479b88c4250dfaf5dc2
yaswanth reddy <sivayaswanth59@gmail.com>
2026-06-29 10:01:05 +0530

    printing all sub-arrays of an Array
    Implement a script to print all subarrays of a given list.

---
+++ Allsubarrays.py
@@ -0,0 +1,9 @@
+# service based asked mostly related to sub arrays 
+
+a = [1,2,3,4] 
+for i in range(len(a)):
+  for j in range(i, len(a)):
+      for k in range(i, j+1):
+         print(a[k], end=" ")
+      print()
+
